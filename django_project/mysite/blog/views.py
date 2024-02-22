2e7f9c44255aff2a8d9bae7cbdaf26ef87ea5c1f
COOVITEL <direcciontop@coovitel.coop>
2024-02-22 16:50:19 -0500

    update django

---
+++ django_project/mysite/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Post
+from django.shortcuts import get_object_or_404
+
+def post_list(request):
+    """"""
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    """"""
+    post_current = get_object_or_404(Post,
+                                     status=Post.Status.PUBLISHED,
+                                     slug=post,
+                                     publish__year=year,
+                                     publish__month=month,
+                                     publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post_current})
