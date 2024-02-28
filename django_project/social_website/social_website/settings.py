096dc1e5cad576b6e07e6f49ea3eaafe99b463f8
COOVITEL <direcciontop@coovitel.coop>
2024-02-28 16:41:28 -0500

    update social web

--- django_project/social_website/social_website/settings.py
+++ django_project/social_website/social_website/settings.py
@@ -31,6 +31,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
