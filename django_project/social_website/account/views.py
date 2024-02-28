ec6e4db1ed261dac8b1677fa2225fa082fe23c92
COOVITEL <direcciontop@coovitel.coop>
2024-02-28 17:02:25 -0500

    up

--- django_project/social_website/account/views.py
+++ django_project/social_website/account/views.py
@@ -54,22 +54,21 @@ def register(request):
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
 
+from django.shortcuts import render
+from .forms import ProfileEditForm
+from django.contrib.auth.decorators import login_required
+
 @login_required
 def edit(request):
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                    data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile,
-                                        data=request.POST,
-                                        files=request.FILES)
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile, created = Profile.objects.get_or_create(user=request.user)
+        profile_form = ProfileEditForm(instance=profile, data=request.POST, files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(
-                        instance=request.user.profile)
-    return render(request,
-                    'account/edit.html',
-                    {'user_form': user_form,
-                       'profile_form': profile_form})
+        profile, created = Profile.objects.get_or_create(user=request.user)
+        profile_form = ProfileEditForm(instance=profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
